9c4f795aaab0172885e14a82e524fc4ac91e8d4e
Andres Vargas <zodman@gmail.com>
2013-04-05 22:48:32 -0600

    dressing

--- example/urls.py
+++ example/urls.py
@@ -3,6 +3,9 @@ from .myshop.views import MyOrderConfirmView
 from shop_simplevariations import urls as simplevariations_urls
 from shop import urls as shop_urls
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf.urls.static import static
+from django.conf import settings
 admin.autodiscover()
 
 urlpatterns = patterns('',
@@ -15,3 +18,7 @@ urlpatterns = patterns('',
 
 
 )
+
+urlpatterns += staticfiles_urlpatterns() + static(settings.MEDIA_URL, 
+	 	document_root=settings.MEDIA_ROOT)
+
