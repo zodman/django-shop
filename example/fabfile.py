24c3961430a85b48a707e59fcd469e5cdf92413c
Andres Vargas <zodman@gmail.com>
2013-04-13 23:15:34 -0500

    update details

---
+++ example/fabfile.py
@@ -0,0 +1,23 @@
+
+from fabric.api import local, settings, show,env, get, run, prompt, cd, lcd
+env.hosts = ["depamap.com"]
+env.user = "zodman"
+PATH = "/home/zodman/disenomx/example"
+
+def __remote_virtualenv(command):
+    VIRTUAL_ENV = "/home/zodman/.virtualenvs/disenomx"
+    source = '. %s/bin/activate && ' % VIRTUAL_ENV
+    run(source + command)
+
+def clear_memcached():
+    run("""echo "flush_all" | nc -q 2 localhost 11211 """)
+
+
+def deploy():
+    with cd(PATH):
+        run("git reset --hard HEAD")
+        run("git pull")
+        __remote_virtualenv("python manage.py migrate")
+        run("touch django.wsgi")
+        clear_memcached()
+
