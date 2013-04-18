e844ffac567f0eeadfa3205c90ecebce7de19fc3
Andres Vargas <zodman@gmail.com>
2013-04-17 22:56:59 -0500

    enhanced menu

--- example/myshop/templatetags/myshop_tags.py
+++ example/myshop/templatetags/myshop_tags.py
@@ -4,6 +4,9 @@ register = template.Library()
 
 from myshop.models import Category
 
-@register.inclusion_tag("shop/menu.html")
-def show_menu():
-	return {'categories':Category.objects.all()}
+
+def show_menu(context):
+
+	return {'categories':Category.objects.all(), 'object':context.get("object")}
+
+register.inclusion_tag("shop/menu.html",takes_context=True)(show_menu)
